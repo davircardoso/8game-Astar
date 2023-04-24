039003259923881fe367aa97268fa4cf53fe1933
davircardoso <davircardoso@gmail.com>
2023-04-23 22:12:06 -0300

    heuristica simples completa

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import time
-from search import Search
+from search import Busca
 
-eight_game = Search()
+jogo8 = Busca()
 
 start_time = time.time()
-move_dezoito = eight_game.largura([[6, 7, 5],[1, 2, 3],[0, 4, 8]])
-move_vintequatro = eight_game.largura([[3, 1, 8],[5, 6, 2],[7, 4, 0]])
+#move_dezoito_largura = jogo8.largura([[6, 7, 5],[1, 2, 3],[0, 4, 8]])
+#move_vintequatro_largura = jogo8.largura([[3, 1, 8],[5, 6, 2],[7, 4, 0]])
+move_vintequatro_manhattan = jogo8.a_estrela_manhattan([[3, 1, 8],[5, 6, 2],[7, 4, 0]])
+move_dezoito_manhattan = jogo8.a_estrela_manhattan([[6, 7, 5],[1, 2, 3],[0, 4, 8]])
+#move_trintaum_manhattan = jogo8.a_estrela_manhattan([[1, 2, 3],[4, 5, 6],[8, 7, 0]])
+move_ss = jogo8.a_estrela_manhattan([[1, 2, 3],[4, 5, 6],[8, 7, 0]])
+move_dezoito_complex = jogo8.a_estrela_heuristica_complexa([[1, 2, 3],[4, 5, 6],[8, 7, 0]])
 end_time = time.time()
-print(move_vintequatro)
+#print('Utilizando Largura para 18 Jogadas', move_dezoito_largura)
+#print('Utilizando Largura para 24 Jogadas', move_vintequatro_largura)
+print('Utilizando Heurística Simples para 18 Jogadas', move_ss)
+#print('Utilizando Heurística Simples para 24 Jogadas', move_vintequatro_manhattan)
+#print('Utilizando Heurística Simples para 31 Jogadas', move_trintaum_manhattan)
+print( 'Utilizando Heurística Complexa para 18 Jogadas', move_dezoito_complex)
 print('Tempo de execução: ', end_time - start_time)
--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-class Search: 
+class Busca: 
        
     def __init__(self):
         self.goal = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]   
@@ -27,6 +27,7 @@ class Search:
                         linha_zero = i
                         col_zero = j
                         break  
+                    
             # Move para cima
             if linha_zero > 0:
                 novo_estado = [linha[:] for linha in estado_atual]
@@ -58,4 +59,150 @@ class Search:
             
         return 'Não foi possível encontrar uma solução'
                 
+                
+    def distancia_manhattan(self, father):
+        distancia = 0
+        for i in range(len(father)):
+            for j in range(len(father[0])):
+                preco = father[i][j]
+                if preco != 0:
+                    linha_zero = (preco - 1) // len(father[0])
+                    col_zero = (preco - 1) % len(father[0])
+                    distancia += abs(linha_zero - i) + abs(col_zero - j)
+        return distancia
+    
+    def distancia_manhattan_melhorada(self, father):
+        dist = 0
+        posicoes = {}
+        for i in range(len(father)):
+            for j in range(len(father[0])):
+                if father[i][j] != 0:
+                    posicoes[father[i][j]] = (i, j)
+        for i in range(len(father)):
+            for j in range(len(father[0])):
+                if father[i][j] != 0:
+                    final_i, final_j = posicoes[father[i][j]]
+                    dist += abs(final_i - i) + abs(final_j - j)
+        return dist
 
+    def a_estrela_manhattan(self, father):
+        linhas = len(father)
+        colunas = len(father[0])
+        linha_zero = 0
+        col_zero = 0
+        explorados = set()
+        abertos = [(father, [])]        
+        while abertos:
+            abertos.sort(key=lambda estado: self.distancia_manhattan(estado[0]) + len(estado[1]) + 1)
+            estado_atual, movimentos = abertos.pop(0)
+            explorados.add(str(estado_atual))
+            
+            if estado_atual == self.goal:
+                qtd_jogadas = 'Quantidade de jogadas: ' + str(len(movimentos))
+                jogadas = 'Jogadas: ' + str(movimentos)
+                busca = 'Número de nodos consultados: ' + str(len(explorados))
+                return qtd_jogadas, jogadas, busca
+            
+            for i in range(linhas):
+                for j in range(colunas):
+                    if estado_atual[i][j] == 0:
+                        linha_zero = i
+                        col_zero = j
+                        break
+                              
+            # Move para cima
+            if linha_zero > 0:
+                novo_estado = [linha[:] for linha in estado_atual]
+                novo_estado[linha_zero][col_zero], novo_estado[linha_zero-1][col_zero] = novo_estado[linha_zero-1][col_zero], novo_estado[linha_zero][col_zero]
+                if str(novo_estado) not in explorados:
+                    custo = self.distancia_manhattan(novo_estado) + len(movimentos) + 1
+                    abertos.append((novo_estado, movimentos + [('Pra cima', novo_estado, custo) ]))
+                
+                
+            # Move para baixo
+            if linha_zero < 2:
+                novo_estado = [linha[:] for linha in estado_atual]
+                novo_estado[linha_zero][col_zero], novo_estado[linha_zero+1][col_zero] = novo_estado[linha_zero+1][col_zero], novo_estado[linha_zero][col_zero]
+                if str(novo_estado) not in explorados:
+                    custo = self.distancia_manhattan(novo_estado) + len(movimentos) + 1
+                    abertos.append((novo_estado, movimentos + [('Pra cima', novo_estado, custo)]))
+                  
+                
+            # Move pra esquerda
+            if col_zero > 0:
+                novo_estado = [linha[:] for linha in estado_atual]
+                novo_estado[linha_zero][col_zero], novo_estado[linha_zero][col_zero-1] = novo_estado[linha_zero][col_zero-1], novo_estado[linha_zero][col_zero]
+                if str(novo_estado) not in explorados:
+                    custo = self.distancia_manhattan(novo_estado) + len(movimentos) + 1
+                    abertos.append((novo_estado, movimentos + [('Pra cima', novo_estado, custo)]))
+                
+            # Move pra direita
+            if col_zero < 2:
+                novo_estado = [linha[:] for linha in estado_atual]
+                novo_estado[linha_zero][col_zero], novo_estado[linha_zero][col_zero+1] = novo_estado[linha_zero][col_zero+1], novo_estado[linha_zero][col_zero]
+                if str(novo_estado) not in explorados:
+                    custo = self.distancia_manhattan(novo_estado) + len(movimentos) + 1
+                    abertos.append((novo_estado, movimentos + [('Pra cima', novo_estado, custo)]))
+            
+        return None
+    
+    def a_estrela_heuristica_complexa(self, father):
+        linhas = len(father)
+        colunas = len(father[0])
+        linha_zero = 0
+        col_zero = 0
+        explorados = set()
+        abertos = [(father, [])]        
+        while abertos:
+            abertos.sort(key=lambda estado: self.distancia_manhattan_melhorada(estado[0]) + len(estado[1]) + 1)
+            estado_atual, movimentos = abertos.pop(0)
+            explorados.add(str(estado_atual))
+            
+            if estado_atual == self.goal:
+                qtd_jogadas = 'Quantidade de jogadas: ' + str(len(movimentos))
+                jogadas = 'Jogadas: ' + str(movimentos)
+                busca = 'Número de nodos consultados: ' + str(len(explorados))
+                return qtd_jogadas, jogadas, busca
+            
+            for i in range(linhas):
+                for j in range(colunas):
+                    if estado_atual[i][j] == 0:
+                        linha_zero = i
+                        col_zero = j
+                        break
+                              
+            # Move para cima
+            if linha_zero > 0:
+                novo_estado = [linha[:] for linha in estado_atual]
+                novo_estado[linha_zero][col_zero], novo_estado[linha_zero-1][col_zero] = novo_estado[linha_zero-1][col_zero], novo_estado[linha_zero][col_zero]
+                if str(novo_estado) not in explorados:
+                    custo = self.distancia_manhattan_melhorada(novo_estado) + len(movimentos) + 1
+                    abertos.append((novo_estado, movimentos + [('Pra cima', novo_estado, custo) ]))
+                
+                
+            # Move para baixo
+            if linha_zero < 2:
+                novo_estado = [linha[:] for linha in estado_atual]
+                novo_estado[linha_zero][col_zero], novo_estado[linha_zero+1][col_zero] = novo_estado[linha_zero+1][col_zero], novo_estado[linha_zero][col_zero]
+                if str(novo_estado) not in explorados:
+                    custo = self.distancia_manhattan_melhorada(novo_estado) + len(movimentos) + 1
+                    abertos.append((novo_estado, movimentos + [('Pra cima', novo_estado, custo)]))
+                  
+                
+            # Move pra esquerda
+            if col_zero > 0:
+                novo_estado = [linha[:] for linha in estado_atual]
+                novo_estado[linha_zero][col_zero], novo_estado[linha_zero][col_zero-1] = novo_estado[linha_zero][col_zero-1], novo_estado[linha_zero][col_zero]
+                if str(novo_estado) not in explorados:
+                    custo = self.distancia_manhattan_melhorada(novo_estado) + len(movimentos) + 1
+                    abertos.append((novo_estado, movimentos + [('Pra cima', novo_estado, custo)]))
+                
+            # Move pra direita
+            if col_zero < 2:
+                novo_estado = [linha[:] for linha in estado_atual]
+                novo_estado[linha_zero][col_zero], novo_estado[linha_zero][col_zero+1] = novo_estado[linha_zero][col_zero+1], novo_estado[linha_zero][col_zero]
+                if str(novo_estado) not in explorados:
+                    custo = self.distancia_manhattan_melhorada(novo_estado) + len(movimentos) + 1
+                    abertos.append((novo_estado, movimentos + [('Pra cima', novo_estado, custo)]))
+            
+        return None
